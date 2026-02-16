f2e537589beab0b13b6dbbfe712d6db705f7dbea
Akinc5 <akshansh9816264920@gmail.com>
2026-02-16 09:46:33 +0530

    sub

---
+++ sub.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        double num1, num2, result;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        num1 = sc.nextDouble();
+
+        System.out.println("Enter the second number:");
+        num2 = sc.nextDouble();
+        result = num1 - num2;
+        System.out.println(num1 + " - " + num2 + " = " + result);
+        sc.close();
+    }
+}
